92124049f08e72996783a9e78da638a490e23754
KlaysW <kotofeyblat@yandex.ru>
2026-06-29 12:04:58 +0300

    Update PZ-12_2.py

--- PZ-12/PZ-12_2.py
+++ PZ-12/PZ-12_2.py
@@ -1,30 +1,23 @@
 # Вариант 32.
-# 2. В матрице найти сумму и произведение элементов строки N (N задать с клавиатуры). reduce
+# 2. В матрице найти сумму и произведение элементов строки N (N задать с клавиатуры).
 
 import random
+from functools import reduce
 
 rows, cols = random.randint(3, 10), random.randint(3, 10)
 matrix = [[random.randint(-10, 10) for _ in range(cols)] for _ in range(rows)]
 
 print("Исходная матрица:")
-for row in matrix:
-    print(row)
+list(map(print, matrix))
 
 n = int(input("\nВведите номер строки N (начиная с 0): "))
 
 if 0 <= n < len(matrix):
-    target_row = matrix[n]
-    
-    row_sum = sum(item for item in target_row)
-    
-    def get_prod(lst):
-        res = 1
-        for x in lst:
-            res *= x
-        return res
-    
-    row_mult = get_prod(target_row)
-        
+    target_row = list(filter(lambda _, i=n: True, matrix))[n]
+
+    row_sum = reduce(lambda acc, x: acc + x, target_row)
+    row_mult = reduce(lambda acc, x: acc * x, target_row)
+
     print(f"\nВыбранная строка {n}: {target_row}")
     print(f"Сумма элементов: {row_sum}")
     print(f"Произведение элементов: {row_mult}")
